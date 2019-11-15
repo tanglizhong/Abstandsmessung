4977bbe9ffc91a9c9ace767d05460fac09bb25b8
Lizhong Tang <tang.lizhong@student.hs-anhalt.de>
2019-11-15 19:49:24 +0100

    上传文件至 ''

--- Abstandsmessung.ino
+++ Abstandsmessung.ino
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////
 //                                                                  //
 // Programmname: Abstandsmessung                                    //
-// Datum: TT.MM.JJJJ                                                //
+// Datum: 15.11.2019                                                //
 // Beschreibung: In diesem Versuch soll ein Ultraschallsensor ange- //
 //               steuert werden. Die gemessene Distanz in cm soll   //
 //               über den seriellen Monitor ausgegeben werden. In   //
@@ -10,8 +10,9 @@
 //               sor kommt, desto schneller blinkt die LED, bis sie //
 //               dauerhaft leuchtet.                                //
 //                                                                  //
-// 1. Autor: Vorname Name, Matrikel, Matrikel-Nr.                   //
-// 2. Autor: Vorname Name, Matrikel, Matrikel-Nr.                   //
+// 1. Autor: Jing    Zhang,   4070426.                  //
+// 2. Autor: Lizhong Tang , 4068443.
+// 3. Autor: Yuqi Han,     4070443.
 //                                                                  //
 //////////////////////////////////////////////////////////////////////
 
@@ -46,7 +47,10 @@ void setup(void) {
   ////////////////////////////////////////////////////////////
   //////////               GPIO Setup               //////////
   ////////////////////////////////////////////////////////////
-
+  Serial.begin(BAUDRATE);
+  pinMode(LED_PIN,OUTPUT);
+  pinMode(US_SENSOR,INPUT);
+  
 
   return; // Beenden der Funktion
 }
@@ -64,6 +68,8 @@ void loop(void) {
   // erstellen Sie eine Variable für die Erfassung der Distanz
   // in Centimeter und eine Variable zur Speicherung des aktu-
   // ellen Delays für die Blinkfrequenz der LED
+  long distanz;
+  int zeit;
 
   ////////////////////////////////////////////////////////////
   //////////              Hauptprogramm             //////////
@@ -71,6 +77,24 @@ void loop(void) {
 
   // fügen Sie hier ihr Hauptprogramm ein
 
+  distanz=US_Sensor.MeasureInCentimeters();
+  Serial.print("Abstand:");
+  Serial.print(distanz);
+  Serial.println("cm");
+  zeit=zeitwert(distanz);
+  if(zeit==0) digitalWrite(LED_PIN,HIGH);
+  else {
+    digitalWrite(LED_PIN,LOW);
+
+    
+
+
+    digitalWrite(LED_PIN,HIGH);
+    delay(zeit);
+    digitalWrite(LED_PIN,LOW);
+    delay(zeit);
+
+  }
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -79,3 +103,16 @@ void loop(void) {
 
 // Erstellen Sie hier eine Unterfunktion, die ihnen einen Delay-Wert 
 // zurück gibt in Abhängigkeit der gemessenen Distanz.
+int zeitwert(long distanz){
+  if(distanz <=10) return 0;
+  if(distanz <=30) return 10;
+  if(distanz <=50) return 20;
+  if(distanz <=100) return 50;
+  if(distanz <=150) return 100;
+  if(distanz <=200) return 250;
+  if(distanz <=300) return 500;
+  else return 1000;
+
+
+
+  }
